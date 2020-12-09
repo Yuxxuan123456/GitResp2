c4bcd362c18d6d3e1c68bbff9aee6eb0fb6e5c9b
YuXuan <309207643@qq.com>
2020-12-09 20:59:39 +0800

    新增了age,height操作

---
+++ TestModule01/src/com/msb/People.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @Auther: zhaoss
+ * @Date: 2020/12/9 - 12 - 09 - 20:58
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class People {
+    int age;
+    int height;
+}
